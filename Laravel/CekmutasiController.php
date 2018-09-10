0972ef0143cae3f9c032e5df81a0c2028d92b3c7
PT-Tridi <43115304+PT-Tridi@users.noreply.github.com>
2018-09-10 21:50:10 +0700

    Update CekmutasiController.php

--- Laravel/CekmutasiController.php
+++ Laravel/CekmutasiController.php
@@ -6,12 +6,15 @@ use PTTridi\Cekmutasi\Cekmutasi;
 
 class CekmutasiController extends Controller
 {
-	$mutasi = Cekmutasi::bank()->mutation([
-			'date'		=> [
-				'from'	=> date('Y-m-d') . ' 00:00:00',
-				'to'	=> date('Y-m-d') . ' 23:59:59'
-			]
-		]);
+	public function index()
+	{
+		$mutasi = Cekmutasi::bank()->mutation([
+				'date'		=> [
+					'from'	=> date('Y-m-d') . ' 00:00:00',
+					'to'	=> date('Y-m-d') . ' 23:59:59'
+				]
+			]);
 
-	dd(json_decode($mutasi));
+		dd(json_decode($mutasi));
+	}
 }
