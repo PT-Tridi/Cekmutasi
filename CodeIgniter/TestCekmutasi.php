35dad27c00ff273ad935a55e3fa60ed4d3507a87
ronywisnuwardana <ronywisnuwardana@gmail.com>
2019-11-25 01:57:42 +0700

    Update codeigniter library

--- CodeIgniter/TestCekmutasi.php
+++ CodeIgniter/TestCekmutasi.php
@@ -2,17 +2,77 @@
 
 class TestCekmutasi extends CI_Controller
 {
-	public function index()
+	public function mutasiBank()
 	{
 		$this->load->library('cekmutasi');
 
-		$mutasi = $this->cekmutasi->bank()->mutation([
+		$mutasi = $this->cekmutasi->bank()->search([
 			'date'		=> [
 				'from'	=> date('Y-m-d') . ' 00:00:00',
 				'to'	=> date('Y-m-d') . ' 23:59:59'
 			]
 		]);
 
-		print_r(json_decode($mutasi));
+		print_r($mutasi);
+	}
+
+	public function mutasiPayPal()
+	{
+		$this->load->library('cekmutasi');
+
+		$mutasi = $this->cekmutasi->paypal()->search([
+			'date'		=> [
+				'from'	=> date('Y-m-d') . ' 00:00:00',
+				'to'	=> date('Y-m-d') . ' 23:59:59'
+			]
+		]);
+
+		print_r($mutasi);
+	}
+
+	public function mutasiOVO()
+	{
+		$this->load->library('cekmutasi');
+
+		$mutasi = $this->cekmutasi->ovo()->search([
+			'date'		=> [
+				'from'	=> date('Y-m-d') . ' 00:00:00',
+				'to'	=> date('Y-m-d') . ' 23:59:59'
+			]
+		]);
+
+		print_r($mutasi);
+	}
+
+	public function mutasiGoPay()
+	{
+		$this->load->library('cekmutasi');
+
+		$mutasi = $this->cekmutasi->gopay()->search([
+			'date'		=> [
+				'from'	=> date('Y-m-d') . ' 00:00:00',
+				'to'	=> date('Y-m-d') . ' 23:59:59'
+			]
+		]);
+
+		print_r($mutasi);
+	}
+
+	public function balance()
+	{
+		$this->load->library('cekmutasi');
+
+		$balance = $this->cekmutasi->balance();
+
+		print_r($balance);
+	}
+
+	public function handleCallback()
+	{
+		$this->load->library('cekmutasi');
+
+		$ipn = $this->cekmutasi->catchIPN();
+
+		print_r($ipn);
 	}
 }
--- CodeIgniter/libraries/Cekmutasi.php
+++ CodeIgniter/libraries/Cekmutasi.php
@@ -6,15 +6,16 @@ class Cekmutasi
 {
 	// place your API Key here
 	private $apiKey = "";
-
-	private $apiUrl = "https://cekmutasi.co.id/v1";
-
+	private $apiSignature = "";
+	private $apiUrl = "https://api.cekmutasi.co.id/v1";
 	public $CI;
+	private $service = null;
 
-	private $service;
-
+	const VERSION = '1.0';
 	const BANK = 1;
 	const PAYPAL = 2;
+	const OVO = 3;
+	const GOPAY = 4;
 
 	public function __construct()
 	{
@@ -37,65 +38,205 @@ class Cekmutasi
 		return $this;
 	}
 
-	private function request($endpoint, $params = [])
+	public function ovo()
 	{
-		$ch = curl_init();
-		curl_setopt_array($ch, [
-			CURLOPT_FRESH_CONNECT	=> true,
-			CURLOPT_URL				=> $this->apiUrl . $endpoint,
-			CURLOPT_RETURNTRANSFER	=> true,
-			CURLOPT_POST			=> true,
-			CURLOPT_POSTFIELDS		=> http_build_query($params),
-			CURLOPT_HEADER			=> false,
-			CURLOPT_HTTPHEADER		=> ['API-KEY: ' . $this->apiKey, 'Accept: application/json'],
-			CURLOPT_SSL_VERIFYHOST	=> 0,
-			CURLOPT_SSL_VERIFYPEER	=> false,
-			CURLOPT_CONNECTTIMEOUT	=> 10,
-			CURLOPT_TIMEOUT 		=> 120,
-			CURLOPT_FAILONERROR		=> true,
-			CURLOPT_IPRESOLVE		=> CURL_IPRESOLVE_V4
-		]);
+		$this->service = self::OVO;
+		return $this;
+	}
 
-		$result = curl_exec($ch);
-		$errno = curl_errno($ch);
+	public function gopay()
+	{
+		$this->service = self::GOPAY;
+		return $this;
+	}
 
-		if( $errno )
+	private function request($endpoint, $method = 'GET', $params = [])
+    {
+    	$url = $this->apiUrl . $endpoint;
+
+    	$ch = curl_init();
+
+    	if( strtoupper($method) == 'GET' )
+    	{
+    		$url .= '?'.http_build_query($params);
+    	}
+    	else
+    	{
+    		curl_setopt($ch, CURLOPT_POST, true);
+    		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+    	}
+
+        curl_setopt($ch, CURLOPT_FRESH_CONNECT, true);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            	'Api-Key: ' . $this->apiKey,
+            	'Accept: application/json',
+                'User-Agent: Cekmutasi CodeIgniter/' . self::VERSION
+            ]);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 180);
+        curl_setopt($ch, CURLOPT_FAILONERROR, false);
+        curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+
+        $result = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $errno = curl_errno($ch);
+        $error = curl_error($ch);
+
+        curl_close($ch);
+
+        if( $errno )
+        {
+            $result = json_encode([
+                'success'     	=> false,
+                'error_message'	=> $error
+            ]);
+        }
+
+        return json_decode($result);
+    }
+
+	public function search(array $options = [])
+	{
+		switch($this->service)
 		{
-			return $this->printOut([
-				'success'			=> false,
-				'error_message'		=> curl_error($ch),
-				'response'			=> [] 
-			]);
+			case self::BANK:
+				$endpoint = '/bank/search';
+				break;
+
+			case self::PAYPAL:
+				$endpoint = '/paypal/search';
+				break;
+
+			case self::OVO:
+				$endpoint = '/ovo/search';
+				break;
+
+			case self::GOPAY:
+				$endpoint = '/gopay/search';
+				break;
+
+			default:
+				throw new \Exception(get_class($this).": Undefined service");
+				break;
 		}
 
-		return $this->printOut(json_decode($result, true));
+		return $this->request($endpoint, 'POST', $options);
 	}
 
-	private function printOut($array)
+	public function transferBankList($sourceNumber)
 	{
-		return json_encode($array);
+		if( $this->service != self::OVO ) {
+			throw new \Exception(get_class($this).": " . __FUNCTION__ . "() method is only available for OVO");
+		}
+
+		return $this->request('/ovo/transfer/bank-list', 'POST', [
+			'source_number'	=> $sourceNumber
+		]);
 	}
 
-	public function mutation($searchOptions = [])
+	public function transferBankInquiry($sourceNumber, $bankCode, $destinationNumber)
 	{
-		$search = [
-			'search'	=> $searchOptions
-		];
-
-		if( $this->service == self::BANK ) {
-			$endpoint = "/bank/search";
-		}
-		elseif( $this->service == self::PAYPAL ) {
-			$endpoint = "/paypal/search";
-		}
-		else {
-			return $this->printOut([
-				'success'		=> false,
-				'error_message'	=> 'Undefined service',
-				'response'		=> []
-			]);
+		if( $this->service != self::OVO ) {
+			throw new \Exception(get_class($this).": " . __FUNCTION__ . "() method is only available for OVO");
 		}
 
-		return $this->request($endpoint, $search);
+		return $this->request('/ovo/transfer/inquiry', 'POST', [
+			'source_number'	=> $sourceNumber,
+			'bank_code'	=> $bankCode,
+			'destination_number'	=> $destinationNumber
+		]);
 	}
+
+	public function transferBank($uuid, $token, $amount, $note = '')
+	{
+		if( $this->service != self::OVO ) {
+			throw new \Exception(get_class($this).": " . __FUNCTION__ . "() method is only available for OVO");
+		}
+
+		return $this->request('/ovo/transfer/send', 'POST', [
+			'uuid'	=> $uuid,
+			'token'	=> $token,
+			'amount'	=> $amount,
+			'note'	=> $note
+		]);
+	}
+
+	public function transferBankDetail($uuid)
+	{
+		if( $this->service != self::OVO ) {
+			throw new \Exception(get_class($this).": " . __FUNCTION__ . "() method is only available for OVO");
+		}
+
+		return $this->request('/ovo/transfer/detail', 'GET', [
+			'uuid'	=> $uuid
+		]);
+	}
+
+	public function transferOVOInquiry($sourceNumber, $destinationNumber)
+	{
+		if( $this->service != self::OVO ) {
+			throw new \Exception(get_class($this).": " . __FUNCTION__ . "() method is only available for OVO");
+		}
+
+		return $this->request('/ovo/transfer/send', 'POST', [
+			'source_number'	=> $sourceNumber,
+			'phone'	=> $destinationNumber
+		]);
+	}
+
+	public function transferOVO($sourceNumber, $destinationNumber, $amount)
+	{
+		if( $this->service != self::OVO ) {
+			throw new \Exception(get_class($this).": " . __FUNCTION__ . "() method is only available for OVO");
+		}
+
+		return $this->request('/ovo/transfer/send', 'POST', [
+			'source_number'	=> $sourceNumber,
+			'phone'	=> $destinationNumber,
+			'amount'	=> $amount
+		]);
+	}
+
+	public function checkIP()
+	{
+		return $this->request('/myip', 'POST');
+	}
+
+	public function balance()
+	{
+		return $this->request('/balance', 'POST');
+	}
+
+	public function catchIPN()
+    {
+        $incomingSignature = $this->CI->input->server('HTTP_API_SIGNATURE') ?: '';
+
+        if( version_compare(PHP_VERSION, '5.6.0', '>=') )
+        {
+            if( !hash_equals($this->apiSignature, $incomingSignature) ) {
+                log_message('info', get_class($this).': Invalid Signature, ' . $this->apiSignature . ' vs ' . $incomingSignature);
+                exit("Invalid signature!");
+            }
+        }
+        else
+        {
+            if( $apiSignature != $incomingSignature ) {
+                log_message('info', get_class($this).': Invalid Signature, ' . $this->apiSignature . ' vs ' . $incomingSignature);
+                exit("Invalid signature!");
+            }
+        }
+
+        $json = $request->getContent();
+        $decoded = json_decode($json);
+
+        if( json_last_error() !== JSON_ERROR_NONE ) {
+            log_message('info', get_class($this).': Invalid JSON, ' . $json);
+            exit("Invalid JSON!");
+        }
+
+        return $decoded;
+    }
 }
